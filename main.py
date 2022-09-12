3cdba8dd24e86bb094051af52bd62561ce08eeae
Sai Krishnan RU <55949052+SaiKrishnan185@users.noreply.github.com>
2022-09-12 10:11:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Created by Sai Krishnan RU
-# 16-12-2021
+# 12-09-2022
 
 # Sort the dataset with population
 # Select values with population limit and having least 1 exclusive currency
@@ -12,16 +12,12 @@ from data import details
 
 import distanceCalculator
 
-# for i in details:
-#     print(i["population"])
 
 currency_data_d = []
 currency_data = []
 population_data = []
 populationLimit = 31028700
 
-# for i in details:
-#     print(i["population"])
 
 # Creating a dataset with unique currencies
 for i in details:
@@ -36,40 +32,23 @@ for i in details:
             currency_data.append(i)
 
 
-# print currency and country name for testing
-# for i in currency_data:
-#     print(i["currencies"][0]["code"], i["currencies"][0]["name"])
 
 # Create new dataset with population limit
 for i in currency_data:
     if i["population"] >= populationLimit:
         population_data.append(i)
 
-# for i in population_data:
-#     print(i["population"])
-
 # Select 20 countries
 final_countries = population_data[0:20]
 # print(len(final_countries))
 
-# for i in final_countries:
-#     print(i["latlng"])
 
 # Calculating sum of distance
 total = 0
 final_countries_copy = final_countries.copy()
 
-# for i in final_countries:
-#     final_countries_copy.remove(i)
-#     # if len(final_countries_copy) != 0:
-#     for j in final_countries_copy:
-#         total = total + (distanceCalculator.distance(i["latlng"][0], i["latlng"][1], j["latlng"][0], j["latlng"][1]))
-# print(round(total, 2))
-
-
 for i in final_countries:
     final_countries_copy.remove(i)
-    # if len(final_countries_copy) != 0:
     for j in final_countries_copy:
         total = total + (distanceCalculator.distance(i["latlng"][0], i["latlng"][1], j["latlng"][0], j["latlng"][1]))
         # break

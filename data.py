96641a95886cf304f042f05a9271f9c7e5f5ecba
Sai Krishnan RU <55949052+SaiKrishnan185@users.noreply.github.com>
2022-11-22 22:22:27 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import json
 
-url = "https://cdn.jsdelivr.net/gh/apilayer/restcountries@3dc0fb110cd97bce9ddf27b3e8e1f7fbe115dc3c/src/main/resources/countriesV2.json"
+url = "link.json"
 
 dataset = urlopen(url)
 details = json.loads(dataset.read())
